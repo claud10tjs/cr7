8e61155aeee0a40bcc8b50bc80589a9e548a32cb
claud10tjs <00001100624466sp@al.educacao.sp.gov.br>
2024-12-03 14:14:55 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'Quantas bolas de ouro o cr7 tem?',
+    '5 bolas'
+)
+
+criaCartao(
+    'futebol',
+    'Qual o maior time do mundo?',
+    'Santos'
+)
+
+criaCartao(
+    'futebol',
+    'Qual e melhor cr7 ou messi?',
+    'cr7'
+)
+
+criaCartao(
+    'comida',
+    'Comeu estronofe hoje?',
+    'sim'
+)
